0910874887c8d62a5f08245107236a2e18795065
SamuelFoo <fooenzesamuel@gmail.com>
2025-03-18 01:24:04 +0800

    Refactor plotting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+from skimage import measure
 
 from Global_Parameter import GlobalParams
 from Layer import Layer
@@ -16,30 +17,66 @@ DATA_DIR.mkdir(exist_ok=True)
 
 PRESENTATION_MEDIA_DIR = Path("presentation_media")
 PRESENTATION_MEDIA_DIR.mkdir(exist_ok=True)
+FIGURE_SIZE = (8, 4)
 
 
-def get_delta_vs_h(_lambda):
+def read_residual_delta_database(query: tuple):
     # Connect to the database
     conn = sqlite3.connect(DATA_DIR / "residual_delta.db")
     c = conn.cursor()
 
     # Read the data from the database
-    c.execute("SELECT h, Delta FROM results WHERE _lambda = ?", (_lambda,))
+    c.execute(*query)
     rows = c.fetchall()
 
     # Close the connection
     conn.close()
 
     # Process the data
-    h_list = []
+    plot_tuples = []
     Deltas = []
+    h_list = []
 
     for row in rows:
-        h, Delta = row
+        h, Delta, x_vals_str, residuals_str = row
         h_list.append(h)
         Deltas.append(Delta)
+        x_vals = eval(x_vals_str)
+        residuals = eval(residuals_str)
+        plot_tuples.append((x_vals, residuals))
 
+    h_list = np.array(h_list)
+    Deltas = np.array(Deltas)
+
+    return h_list, Deltas, plot_tuples
+
+
+def get_delta_vs_h(_lambda):
+    query = (
+        "SELECT h, Delta, x_vals, residuals FROM results WHERE _lambda = ?",
+        (_lambda,),
+    )
+    h_list, Deltas, _ = read_residual_delta_database(query)
     return h_list, Deltas
+
+
+def get_residuals(_lambda, Delta_list, h_list):
+    residual_list = []
+
+    for Delta, h in zip(Delta_list, h_list):
+        conn = sqlite3.connect(DATA_DIR / "residuals.db")
+        c = conn.cursor()
+
+        c.execute(
+            f"SELECT * FROM residuals WHERE lambda={_lambda} AND Delta={Delta} AND h={h}"
+        )
+        query = c.fetchone()
+        residual = query[3]
+        residual_list.append(residual)
+
+        conn.close()
+
+    return residual_list
 
 
 def run_for_lambda(_lambda, h_end=1e-3, delta_end=2e-3):
@@ -103,57 +140,87 @@ def run_for_lambda(_lambda, h_end=1e-3, delta_end=2e-3):
 
 
 def plot_for_lambda(_lambda):
-    # Connect to the database
-    conn = sqlite3.connect(DATA_DIR / "residual_delta.db")
-    c = conn.cursor()
-
-    # Read the data from the database
-    c.execute(
-        "SELECT h, Delta, x_vals, residuals FROM results WHERE _lambda = ?", (_lambda,)
+    query = (
+        "SELECT h, Delta, x_vals, residuals FROM results WHERE _lambda = ?",
+        (_lambda,),
     )
-    rows = c.fetchall()
+    h_list, Deltas, plot_tuples = read_residual_delta_database(query)
 
-    # Close the connection
-    conn.close()
+    fig = plt.figure(figsize=FIGURE_SIZE)
+    ax = fig.add_subplot()
 
-    # Process the data
-    plot_tuples = []
-    Deltas = []
-    h_list = []
-
-    for row in rows:
-        h, Delta, x_vals_str, residuals_str = row
-        h_list.append(h)
-        Deltas.append(Delta)
-        x_vals = eval(x_vals_str)
-        residuals = eval(residuals_str)
-        plot_tuples.append((x_vals, residuals))
-
-    plt.figure(figsize=(12, 6))
-
-    sc = plt.scatter([], [], c=[], cmap="copper", vmin=min(h_list), vmax=max(h_list))
-    cbar = plt.colorbar(sc)
-    cbar.set_label("h (eV)")
+    sc = ax.scatter(
+        [], [], c=[], cmap="copper", vmin=min(h_list) * 1e3, vmax=max(h_list) * 1e3
+    )
+    cbar = fig.colorbar(sc)
+    cbar.set_label("h (meV)")
 
     for h, (x_vals, residuals), Delta in zip(h_list, plot_tuples, Deltas):
         color = plt.cm.copper((h - min(h_list)) / (max(h_list) - min(h_list)))
-        plt.plot(x_vals, residuals, color=color)
-        plt.scatter([Delta], [0], color=color, marker="x")
+        x_vals_meV = np.array(x_vals) * 1e3
+        residuals_meV = np.array(residuals) * 1e3
+        ax.plot(x_vals_meV, residuals_meV, color=color)
+        ax.scatter([Delta * 1e3], [0], color=color, marker="x")
 
-    plt.scatter([], [], color="black", marker="x", label="Root")
+    ax.scatter([], [], color="black", marker="x", label="Root")
 
-    plt.axhline(y=0, color="gray", linestyle="--")
-    plt.xlabel(r"$\Delta_0$ (eV)")
-    plt.ylabel("Residual (eV)")
-    plt.title(rf"$\lambda = {_lambda}$")
-    plt.legend()
-    plt.savefig(
-        PRESENTATION_MEDIA_DIR / f"residuals_delta_lambda_{_lambda}.svg",
-        transparent=True,
-        bbox_inches="tight",
-        pad_inches=None,
+    ax.axhline(y=0, color="gray", linestyle="--")
+    ax.set_xlabel(r"$\Delta_0$ (meV)")
+    ax.set_ylabel("Residual (meV)")
+    ax.set_title(rf"$\lambda = {_lambda}$")
+    ax.legend()
+    return fig
+
+
+def plot_for_lambda_zeros(_lambda):
+    h_list, Deltas = get_delta_vs_h(_lambda)
+
+    fig = plt.figure(figsize=FIGURE_SIZE)
+    ax = fig.add_subplot()
+
+    sc = ax.scatter(
+        [], [], c=[], cmap="copper", vmin=min(h_list) * 1e3, vmax=max(h_list) * 1e3
     )
-    plt.show()
+    cbar = fig.colorbar(sc)
+    cbar.set_label("h (meV)")
+
+    for h, Delta in zip(h_list, Deltas):
+        color = plt.cm.copper((h - min(h_list)) / (max(h_list) - min(h_list)))
+        ax.scatter([Delta * 1e3], [0], color=color, marker="x")
+
+    ax.scatter([], [], color="black", marker="x", label="Root")
+
+    ax.axhline(y=0, color="gray", linestyle="--")
+    ax.set_xlabel(r"$\Delta_0$ (meV)")
+    ax.set_ylabel("Residual (meV)")
+    ax.set_title(rf"$\lambda = {_lambda}$")
+    ax.legend()
+    return fig
+
+
+def plot_for_lambda_h_list(_lambda, h_list):
+    query = (
+        "SELECT h, Delta, x_vals, residuals FROM results WHERE _lambda = ? AND h IN ({seq})".format(
+            seq=",".join(["?"] * len(h_list))
+        ),
+        [_lambda] + h_list,
+    )
+    h_list, Deltas, plot_tuples = read_residual_delta_database(query)
+
+    fig = plt.figure(figsize=FIGURE_SIZE)
+    ax = fig.add_subplot()
+
+    for h, (x_vals, residuals), Delta in zip(h_list, plot_tuples, Deltas):
+        x_vals_meV = np.array(x_vals) * 1e3
+        residuals_meV = np.array(residuals) * 1e3
+        ax.plot(x_vals_meV, residuals_meV, label=f"h = {h * 1e3} meV")
+
+    ax.axhline(y=0, color="gray", linestyle="--")
+    ax.set_xlabel(r"$\Delta_0$ (meV)")
+    ax.set_ylabel("Residual (meV)")
+    ax.set_title(rf"$\lambda = {_lambda}$")
+    ax.legend()
+    return fig
 
 
 def drop_lambda(_lambda):
@@ -162,3 +229,13 @@ def drop_lambda(_lambda):
     c.execute("DELETE FROM results WHERE _lambda = ?", (_lambda,))
     conn.commit()
     conn.close()
+
+
+def get_contour(
+    x_mesh: np.ndarray, y_mesh: np.ndarray, z_mesh: np.ndarray, value: np.float64
+):
+    contours = measure.find_contours(z_mesh, value)
+    contour = contours[0]
+    x_scale = 1 / x_mesh.shape[1] * x_mesh.max()
+    y_scale = 1 / y_mesh.shape[1] * y_mesh.max()
+    return contour[:, 1] * x_scale, contour[:, 0] * y_scale
